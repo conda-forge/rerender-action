f22c57efd5b256bad03294be90791f535f0e3d0a
beckermr <becker.mr@gmail.com>
2024-09-29 05:45:07 -0500

    refactor: one routine for rerendering

--- webservices_dispatch_action/__main__.py
+++ webservices_dispatch_action/__main__.py
@@ -51,6 +51,61 @@ def _pull_docker_image():
         print("::endgroup::", flush=True)
 
 
+def _do_rerender(git_repo, pr_branch, pr_owner, pr_repo, repo_name, pr):
+    # rerender
+    _, _, can_change_workflows = get_actor_token()
+    can_change_workflows = (
+        can_change_workflows or os.environ["HAS_SSH_PRIVATE_KEY"] == "true"
+    )
+    changed, rerender_error, info_message = rerender(git_repo, can_change_workflows)
+
+    # comment
+    more_info_message = """\
+\nThe following suggestions might help debug any issues:
+* Is the `recipe/{{meta.yaml,recipe.yaml}}` file valid?
+* If there is a `recipe/conda-build-config.yaml` file in the feedstock make sure
+  that it is compatible with the current [global pinnnings]({}).
+* Is the fork used for this PR on an organization or user GitHub account? \
+Automated rerendering via the webservices admin bot only works for user \
+GitHub accounts.
+""".format(
+        "https://github.com/conda-forge/conda-forge-pinning-feedstock/"
+        "blob/master/recipe/conda_build_config.yaml"
+    )
+    if rerender_error:
+        if info_message is None:
+            info_message = ""
+        info_message += more_info_message
+
+    push_error = comment_and_push_if_changed(
+        action="rerender",
+        changed=changed,
+        error=rerender_error,
+        git_repo=git_repo,
+        pull=pr,
+        pr_branch=pr_branch,
+        pr_owner=pr_owner,
+        pr_repo=pr_repo,
+        repo_name=repo_name,
+        close_pr_if_no_changes_or_errors=False,
+        help_message=" or you can try [rerendering locally](%s)"
+        % (
+            "https://conda-forge.org/docs/maintainer/updating_pkgs.html"
+            "#rerendering-with-conda-smithy-locally"
+        ),
+        info_message=info_message,
+    )
+
+    if rerender_error or push_error:
+        raise RuntimeError(
+            "Rerendering failed! error in push|rerender: %s|%s"
+            % (
+                push_error,
+                rerender_error,
+            ),
+        )
+
+
 def main():
     logging.basicConfig(level=logging.INFO)
 
@@ -104,60 +159,9 @@ def main():
                 )
 
                 # rerender
-                _, _, can_change_workflows = get_actor_token()
-                can_change_workflows = (
-                    can_change_workflows or os.environ["HAS_SSH_PRIVATE_KEY"] == "true"
-                )
                 _pull_docker_image()
-                changed, rerender_error, info_message = rerender(
-                    git_repo, can_change_workflows
-                )
+                _do_rerender(git_repo, pr_branch, pr_owner, pr_repo, repo_name, pr)
 
-                # comment
-                more_info_message = """\
-\nThe following suggestions might help debug any issues:
-* Is the `recipe/{{meta.yaml,recipe.yaml}}` file valid?
-* If there is a `recipe/conda-build-config.yaml` file in the feedstock make sure
-  that it is compatible with the current [global pinnnings]({}).
-* Is the fork used for this PR on an organization or user GitHub account? \
-Automated rerendering via the webservices admin bot only works for user \
-GitHub accounts.
-""".format(
-                    "https://github.com/conda-forge/conda-forge-pinning-feedstock/"
-                    "blob/master/recipe/conda_build_config.yaml"
-                )
-                if rerender_error:
-                    if info_message is None:
-                        info_message = ""
-                    info_message += more_info_message
-
-                push_error = comment_and_push_if_changed(
-                    action="rerender",
-                    changed=changed,
-                    error=rerender_error,
-                    git_repo=git_repo,
-                    pull=pr,
-                    pr_branch=pr_branch,
-                    pr_owner=pr_owner,
-                    pr_repo=pr_repo,
-                    repo_name=repo_name,
-                    close_pr_if_no_changes_or_errors=False,
-                    help_message=" or you can try [rerendering locally](%s)"
-                    % (
-                        "https://conda-forge.org/docs/maintainer/updating_pkgs.html"
-                        "#rerendering-with-conda-smithy-locally"
-                    ),
-                    info_message=info_message,
-                )
-
-                if rerender_error or push_error:
-                    raise RuntimeError(
-                        "Rerendering failed! error in push|rerender: %s|%s"
-                        % (
-                            push_error,
-                            rerender_error,
-                        ),
-                    )
                 # if the pr was made by the bot, mark it as ready for review
                 if pr.title == "MNT: rerender" and pr.user.login == "conda-forge-admin":
                     mark_pr_as_ready_for_review(pr)
@@ -234,37 +238,8 @@ GitHub accounts.
                     )
 
                 if version_changed:
-                    # rerender
-                    rerender_changed, rerender_error, info_message = rerender(
-                        git_repo, can_change_workflows
-                    )
-                    rerender_push_error = comment_and_push_if_changed(
-                        action="rerender",
-                        changed=rerender_changed,
-                        error=rerender_error,
-                        git_repo=git_repo,
-                        pull=pr,
-                        pr_branch=pr_branch,
-                        pr_owner=pr_owner,
-                        pr_repo=pr_repo,
-                        repo_name=repo_name,
-                        close_pr_if_no_changes_or_errors=False,
-                        help_message=" or you can try [rerendering locally](%s)"
-                        % (
-                            "https://conda-forge.org/docs/maintainer/updating_pkgs.html"
-                            "#rerendering-with-conda-smithy-locally"
-                        ),
-                        info_message=info_message,
-                    )
+                    _do_rerender(git_repo, pr_branch, pr_owner, pr_repo, repo_name, pr)
 
-                    if rerender_error or rerender_push_error:
-                        raise RuntimeError(
-                            "Rerendering failed! error in push|rerender: %s|%s"
-                            % (
-                                push_error,
-                                rerender_error,
-                            ),
-                        )
                     if found_version:
                         LOGGER.info(
                             "Updating PR title for %s#%s with version=%s",
